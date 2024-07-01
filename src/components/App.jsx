c88bfca95828b01ec886f0d8f974ee64955eab4f
roel.edeleon <roel.edeleon@gmail.com>
2024-07-01 19:08:57 +0800

    20240701 1908H
    Transaction History - done!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,16 @@ import { Statistics } from './Statistics/Statistics';
 import friends from '../data/friends.json';
 import { FriendList } from './FriendList/FriendList';
 
+import transactions from '../data/transactions.json';
+import { TransactionHistory } from './TransactionHistory/TrasactionHistory';
+
 export const App = () => {
   return (
     <>
       <Profile {...user}></Profile>
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
---
+++ src/components/TransactionHistory/TrasactionHistory.jsx
@@ -0,0 +1,26 @@
+import css from './TransactionHistory.modules.css';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <>
+      <table className={css.table}>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map(({ id, type, amount, currency }) => (
+            <tr key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
